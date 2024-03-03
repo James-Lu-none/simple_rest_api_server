0afa7f46eba2ffbdfbb35a76a38011fc4188007d
James-Lu-none <jamesabcde277@gmail.com>
2024-03-03 16:45:30 +0800

    update gitignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,90 @@
+import functions = require("firebase-functions");
+import admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert("serviceAccountKey.json"),
+});
+
+import express = require("express");
+import cors = require("cors");
+const db = admin.firestore();
+const pcStatusCollection = db.collection("pc_status");
+const app = express();
+app.use(cors({origin: true}));
+
+app.get("/", (req, res) => {
+  return res.status(200).send("Hi");
+});
+
+app.get("/api/pc_status/get", (req, res) => {
+  (async () => {
+    try {
+      const pcId = req.query.pc_id;
+      console.log(pcId);
+      if (pcId === null || typeof pcId !== "string") {
+        throw Error("pc id was invalid");
+      }
+      // const data = await pc_status_collection.doc(pcId).get();
+      let response;
+      await pcStatusCollection
+        .doc(pcId)
+        .get()
+        .then((data) => {
+          response = data.data();
+          return response;
+        });
+      return res
+        .status(200)
+        .send({status: "Success", data: response, msg: "Data retrieved"});
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({status: "Failed", msg: error});
+    }
+  })();
+});
+
+app.put("/api/pc_status/set", (req, res) => {
+  (async () => {
+    console.log(req.params);
+    console.log(req.body);
+    try {
+      let data = {};
+      const pcId = req.query.pc_id;
+      console.log(pcId);
+      if (pcId === null || typeof pcId !== "string") {
+        throw Error("pc id was invalid");
+      }
+      data = assignBooleanIfNotNullOrUndefined("isPcOn", req.body.isPcOn, data);
+      data = assignBooleanIfNotNullOrUndefined(
+        "powerOnTriggerFlag",
+        req.body.powerOnTriggerFlag,
+        data
+      );
+      console.log(data);
+      await pcStatusCollection.doc(pcId).update(data);
+      return res.status(200).send({status: "Success", msg: "Data updated"});
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({status: "Failed", msg: error});
+    }
+  })();
+});
+
 /**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * @param {string} property - The property name to be assigned.
+ * @param {boolean} value - The boolean value to be assigned.
+ * @param {object} data - The object to which the boolean property is assigned.
+ * @return {object} - The updated object.
  */
+function assignBooleanIfNotNullOrUndefined(
+  property: string,
+  value: boolean,
+  data: object
+): object {
+  if (value !== null && value !== undefined && typeof value === "boolean") {
+    data = Object.assign({[property]: value}, data);
+  }
+  return data;
+}
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
